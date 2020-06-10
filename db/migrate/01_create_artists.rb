0dc99536015f4881839b39dfafe39b8cbb977014
Irene Scott <irenejinscott@gmail.com>
2020-06-10 14:02:38 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -4,4 +4,9 @@ class CreateArtists < ActiveRecord::Migration[5.2]
 
     def down
     end
+
+    ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/artists.sqlite"
+    )
 end
